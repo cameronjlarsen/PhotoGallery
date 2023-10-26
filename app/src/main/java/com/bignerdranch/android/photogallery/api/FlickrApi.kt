76b9e259b7e0edf25ef1437483b68a5f0e397a86
Cameron Larsen <camjlarsen@gmail.com>
2023-10-25 23:44:26 -0700

    feat: searching for photos

--- app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -2,14 +2,14 @@ package com.bignerdranch.android.photogallery.api
 
 import com.bignerdranch.android.photogallery.BuildConfig
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 private const val API_KEY = BuildConfig.APIKEY
 
 interface FlickrApi {
-    @GET("services/rest/?method=flickr.interestingness.getList" +
-            "&api_key=$API_KEY" +
-            "&format=json" +
-            "&nojsoncallback=1" +
-            "&extras=url_s")
+    @GET("services/rest/?method=flickr.interestingness.getList")
     suspend fun fetchPhotos(): FlickrResponse
+
+    @GET("services/rest/?method=flickr.photos.search")
+    suspend fun searchPhotos(@Query("text") query: String): FlickrResponse
 }
